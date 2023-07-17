c2d401f6132680daa699b6453277e93df5fe577a
hossainarif37 <hossainarif4937@gmail.com>
2023-07-17 16:40:18 +0600

    added react helmet package for seo and modify a component for test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./shared/Navbar";
+import { Helmet } from "react-helmet";
 
 const App = () => {
   return (
     <div className="max-w-[1440px] mx-auto overflow-hidden">
+      <Helmet>
+        <title>Luxury Living</title>
+        <meta name="description" content="This is Luxury Living." />
+        <meta name="keywords" content="luxury living, luxury, Luxury Living, Arif Luxury Living" />
+      </Helmet>
       <Navbar />
       <Outlet />
     </div>
