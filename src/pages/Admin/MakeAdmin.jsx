e512731c5efe59881c58a1ea90f4183572d8b237
hossainarif37 <hossainarif4937@gmail.com>
2023-07-28 05:14:28 +0600

    fixed make admin page responsive problem

--- src/pages/Admin/MakeAdmin.jsx
+++ src/pages/Admin/MakeAdmin.jsx
@@ -2,10 +2,10 @@
 const MakeAdmin = () => {
     return (
         <div className=" h-screen  mr-10 ml-3 mt-5 ">
-            <div className="bg-white h-1/2 p-5 rounded-2xl">
+            <div className="bg-white h-1/3 lg:h-1/2 p-5 rounded-2xl">
                 <label className="" htmlFor="make-admin-input">Email</label>
-                <div className="flex gap-5 mt-3">
-                    <input className="input w-1/2 border bg-white" type="email" placeholder="example@gmail.com" />
+                <div className="flex flex-col lg:flex-row gap-5 mt-3">
+                    <input className="input lg:w-1/2 border bg-white" type="email" placeholder="example@gmail.com" />
                     <button className="btn">Submit</button>
                 </div>
             </div>
