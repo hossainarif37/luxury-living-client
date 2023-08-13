d9bce11656aa9d4b59bd35c2b8a5cf0188d833d5
hossainarif37 <hossainarif4937@gmail.com>
2023-08-13 23:46:51 +0600

    fix a bug of hashrouter

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import Contact from "./Contact";
 import Hero from "./Hero";
 import Projects from "./Projects";
 import Services from "./Services";
 import Testimonials from "./Testimonials";
+import { BsFillArrowUpSquareFill } from 'react-icons/bs'
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const handleAchorLink = async (e, hashroute) => {
+        e.preventDefault();
+        await navigate('/');
+        window.location.hash = hashroute;
+    }
     return (
         <div>
             <Hero />
@@ -12,6 +21,9 @@ const Home = () => {
             <Services />
             <Testimonials />
             <Contact />
+            <a href='' onClick={(e) => handleAchorLink(e, 'home')} className="text-4xl fixed bottom-5 right-3">
+                <BsFillArrowUpSquareFill />
+            </a>
         </div>
     );
 };
